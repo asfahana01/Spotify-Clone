ffe5ee6b84777ebab5623afaa6f3ec22968f4653
asfahana01 <hana17asfa@gmail.com>
2026-01-28 18:57:05 +0530

    play the 1st song

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-async function main(){
-let a =await fetch("http://127.0.0.1:5500/songs/")
-const response = await a.text()
-console.log(response)
-let div = document.createElement("div")
-dispatchEvent.innerHTML = response;
-let tds = div.getElementsByTagName("td")
-console.log(tds)
-}
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:5500/songs/")
+    const response = await a.text()
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
 
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href)
+        }
+    }
+    return songs
+}
+async function main() {
+    // get the list of all songs
+    let songs = await getSongs()
+    console.log(songs)
+
+    // play the first songs
+    var audio = new Audio(songs[0])
+    audio.play()
+
+    audio.addEventListener("loadeddata", () => {
+        let duration = audio.duration
+        console.log(audio.duration, audio.currentSrc, audio.currentTime)
+        // The duration variable now holds the duration (in seconds) of the audio clip
+    });
+}
 main()
