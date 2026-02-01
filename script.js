964da494ef4383418c53a1805260aef687f35c80
asfahana01 <hana17asfa@gmail.com>
2026-02-01 12:12:26 +0530

    opening and closing of the left sideBar
    Updated in responsive part

--- script.js
+++ script.js
@@ -99,5 +99,14 @@ async function main() {
         document.querySelector(".circle").style.left = percent + "%"
         currentSong.currentTime = ((currentSong.duration) * percent) / 100
     })
+
+    //add ann event listener to hamburger
+    document.querySelector(".hamburger").addEventListener("click",()=>{
+        document.querySelector(".left").style.left = "0"
+    })
+      //add ann event listener to close button
+     document.querySelector(".close").addEventListener("click",()=>{
+        document.querySelector(".left").style.left = "-100%"
+    })
 }
 main()
